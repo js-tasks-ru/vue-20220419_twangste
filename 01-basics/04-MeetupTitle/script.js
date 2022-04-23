eef0fd1ded3d985162f01456ec48ad8705ddb52f
Alexey Struchenkov <AStruchenkov@gmail.com>
2022-04-23 23:09:25 +0200

    01-basics/04-MeetupTitle solved

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,36 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+const RootComponent = defineComponent({
+  data() {
+    return {
+      meetups: [],
+      activeMeetupId: 1,
+    };
+  },
+  computed: {
+    currentMeetup() {
+      return this.meetups.find((item) => item.id === this.activeMeetupId);
+    },
+  },
+  watch: {
+    activeMeetupId: function (id) {
+      if (id && !this.currentMeetup) this.getMeetUpdata(id);
+    },
+  },
+  methods: {
+    async getMeetUpdata(id) {
+      if (!id) return;
+      const index = this.meetups.findIndex((item) => item.id === id);
+      if (index === -1) {
+        const response = await fetchMeetupById(id);
+        if (response) this.meetups.push(response);
+      }
+    },
+  },
+  mounted() {
+    this.getMeetUpdata(this.activeMeetupId);
+  },
+});
+
+const app = createApp(RootComponent).mount('#app');
