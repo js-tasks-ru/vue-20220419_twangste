cbcb50a7ad8aa5c9b9e9dc2c161db2c77c0fe83b
Alexey Struchenkov <AStruchenkov@gmail.com>
2022-04-23 16:58:05 +0200

    01-basics/02-VueCalculator solved

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Root = defineComponent({
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    result() {
+      const { a, b } = this;
+      switch (this.operator) {
+        case 'sum':
+          return a + b;
+        case 'subtract':
+          return a - b;
+        case 'multiply':
+          return a * b;
+        case 'divide':
+          return b !== 0 ? a / b : NaN;
+      }
+    },
+  },
+});
+
+const app = createApp(Root).mount('#app');
